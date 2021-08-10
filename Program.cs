badb9eab2947848b790a74259ebf5db9a2cd3bb8
SSGSSACE <quangbayhai@gmail.com>
2021-08-10 21:13:48 +0700

    August 10, 2021 9:13 PM

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Data;
+// using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
+// using System.Text;
 // using System.Text.RegularExpressions;
 namespace ConsoleApp
 {
